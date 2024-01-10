8bdd64d486e6345f8d87f6dc0b898990bba9fcbf
Vita Filimonova <vitka.2001@bk.ru>
2024-01-10 16:17:05 +0300

    do bem refactoring

--- js/main.js
+++ js/main.js
@@ -2,98 +2,92 @@ const buttonSales = document.querySelectorAll(".button-sales");
 const cookiesButtonAccept = document.querySelector("#accept-cookies");
 const cookiesButtonDecline = document.querySelector("#decline-cookies");
 // const cookiesButtonClose = document.querySelector(".cookies__close-button");
-const cookies = document.querySelector("#cookies");
+const cookies = document.querySelector(".cookies");
 const contactsTab = document.querySelector(".contacts-tab");
 const buttonSubmitForm = document.querySelector(".submit");
 const thanksForm = document.querySelector(".thanks-tab");
-const closeButton = document.querySelectorAll('.close-button')
+const closeButton = document.querySelectorAll(".close-button");
 
-const requiredFields = document.querySelectorAll(".required-value")
-const superButton = document.querySelector(".super-button")
-const overlay = document.querySelector(".overlay")
-const error = document.querySelectorAll(".error")
+const requiredFields = document.querySelectorAll(".required-value");
+const superButton = document.querySelector(".button--super");
+const overlay = document.querySelector(".overlay");
+const error = document.querySelectorAll(".contacts-form__error");
 
-const closeBurgerMenu = document.querySelector(".close-burger-menu")
-const burgerButton = document.querySelector(".burger-button")
+const closeMobileMenu = document.querySelector(".mobile-menu__close-button");
+const burgerButton = document.querySelector(".burger-button");
 
-const mobileMenu = document.querySelector(".mobile-menu__container")
-const globalError = document.querySelector(".global-error")
-const submitButton = document.querySelector(".submit")
+const mobileMenu = document.querySelector(".mobile-menu__container");
+const globalError = document.querySelector(".contacts-form__global-error");
+const submitButton = document.querySelector(".button-submit");
 
 const inputs = document.querySelectorAll("input");
 buttonSales.forEach((button) => {
-    button.addEventListener("click", () => {
-        // const modal = document.querySelector(button.dataset.modalTarget);
-        openContactsTab();
-    });
+  button.addEventListener("click", () => {
+    // const modal = document.querySelector(button.dataset.modalTarget);
+    openContactsTab();
+  });
 });
 
 closeButton.forEach(function (button) {
-    button.addEventListener("click",
-        function () {
-            // Получаем родительский элемент кнопки и добавляем класс "close"
-            const parent = button.parentElement;
-            parent.classList.remove("open");
-            parent.classList.add("close");
-            overlay.classList.remove("overlay-active")
-            document.body.style.overflow = 'auto';
-
-        });
+  button.addEventListener("click", function () {
+    // Получаем родительский элемент кнопки и добавляем класс "close"
+    const parent = button.parentElement;
+    parent.classList.remove("open");
+    parent.classList.add("close");
+    overlay.classList.remove("overlay-active");
+    document.body.style.overflow = "auto";
+  });
 });
-
 
 cookiesButtonDecline.addEventListener("click", closeCookies);
 cookiesButtonAccept.addEventListener("click", closeCookies);
 
-
 // buttonSubmitForm.addEventListener("click", openThanksTab)
 
 burgerButton.addEventListener("click", function () {
-    mobileMenu.classList.add('open')
-    document.body.style.overflow = 'hidden';
-})
-closeBurgerMenu.addEventListener("click", function () {
-    mobileMenu.classList.remove('open')
-    document.body.style.overflow = 'auto';
-})
+  mobileMenu.classList.add("open");
+  document.body.style.overflow = "hidden";
+});
+closeMobileMenu.addEventListener("click", function () {
+  mobileMenu.classList.remove("open");
+  document.body.style.overflow = "auto";
+});
 // const requiredInputs = document.querySelectorAll(".required-value")
-
 
 const requiredInputs = document.querySelectorAll(".required-value");
 
 function checkRequiredInputs() {
-    return [...requiredInputs].every((input) =>
-        input.value.trim().length !== 0 && phoneInput.value.length ===16
-    );
+  return [...requiredInputs].every(
+    (input) =>
+      input.value.trim().length !== 0 && phoneInput.value.length === 16,
+  );
 }
 
 function toggleErrorElement(input) {
-    const errorElement = input.nextElementSibling;
-    errorElement.classList.toggle("open", input.value.trim().length === 0);
-    if (errorElement.className === "error open") {
-        input.classList.add("input-error");
-    } else {
-        input.classList.remove("input-error");
-    }
+  const errorElement = input.nextElementSibling;
+  errorElement.classList.toggle("open", input.value.trim().length === 0);
+  if (errorElement.className === "contacts-form__error open") {
+    input.classList.add("input-error");
+  } else {
+    input.classList.remove("input-error");
+  }
 }
 
 requiredInputs.forEach((input) => {
-    input.addEventListener("focusout", () => {
-        if (phoneInput.value.length >= 16) {
-            toggleErrorElement(input);
-        }
+  input.addEventListener("focusout", () => {
+    if (phoneInput.value.length >= 16) {
+      toggleErrorElement(input);
+    }
 
-        const hasEmptyRequiredFields = [...requiredInputs].some(
-            (input) => input.value.trim().length === 0
-        );
+    const hasEmptyRequiredFields = [...requiredInputs].some(
+      (input) => input.value.trim().length === 0,
+    );
 
-        const globalErrorElement = document.querySelector(".global-error");
-        globalErrorElement.classList.toggle("open", hasEmptyRequiredFields);
-        buttonSubmitForm.disabled = !checkRequiredInputs();
-    });
+    const globalErrorElement = document.querySelector(".global-error");
+    globalErrorElement.classList.toggle("open", hasEmptyRequiredFields);
+    buttonSubmitForm.disabled = !checkRequiredInputs();
+  });
 });
-
-
 
 // function checkRequiredInputs() {
 //     return [...requiredInputs].every((input) =>
@@ -126,98 +120,79 @@ requiredInputs.forEach((input) => {
 //     });
 // });
 
-
 submitButton.addEventListener("click", (event) => {
-    event.preventDefault()
-    inputs.forEach((input) => {
-        input.value = ""; // Устанавливаем значение инпута в пустую строку
-    });
+  event.preventDefault();
+  inputs.forEach((input) => {
+    input.value = ""; // Устанавливаем значение инпута в пустую строку
+  });
 
+  buttonSubmitForm.disabled = true;
 
-    buttonSubmitForm.disabled = true
-
-
-    openThanksTab()
-})
+  openThanksTab();
+});
 
 superButton.addEventListener("click", function () {
-    thanksForm.classList.remove("open");
-    overlay.classList.remove("overlay-active")
-    document.body.style.overflow = 'auto';
-})
+  thanksForm.classList.remove("open");
+  overlay.classList.remove("overlay-active");
+  document.body.style.overflow = "auto";
+});
 
 function closeCookies() {
-    cookies.classList.add("close");
+  cookies.classList.add("close");
 }
 
 function openContactsTab() {
-    contactsTab.classList.add("open");
-    overlay.classList.add("overlay-active")
-    document.body.style.overflow = 'hidden'; // Блокируем скроллинг страницы
+  contactsTab.classList.add("open");
+  overlay.classList.add("overlay-active");
+  document.body.style.overflow = "hidden"; // Блокируем скроллинг страницы
 }
 
 function openThanksTab() {
-    contactsTab.classList.remove("open");
-    thanksForm.classList.add("open")
-
+  contactsTab.classList.remove("open");
+  thanksForm.classList.add("open");
 }
 
-
-
-
-
-
-
-
-
-const phoneInput = document.querySelector("#phone-number")
+const phoneInput = document.querySelector("#phone-number");
 
 const prefixNumber = (str) => {
-
-    return "+7 ";
+  return "+7 ";
 };
 
 phoneInput.addEventListener("input", (e) => {
-    const value = phoneInput.value.replace(/\D+/g, "");
-    const numberLength = 11;
+  const value = phoneInput.value.replace(/\D+/g, "");
+  const numberLength = 11;
 
-    let result = '';
+  let result = "";
 
-    for (let i = 0; i < value.length && i < numberLength; i++) {
-        switch (i) {
-            case 0:
-                result += prefixNumber(value[i]);
-                continue;
-            case 4:
-                result += " ";
-                break;
-            case 7:
-                result += " ";
-                break;
-            case 9:
-                result += " ";
-                break;
-            default:
-                break;
-        }
-        result += value[i];
+  for (let i = 0; i < value.length && i < numberLength; i++) {
+    switch (i) {
+      case 0:
+        result += prefixNumber(value[i]);
+        continue;
+      case 4:
+        result += " ";
+        break;
+      case 7:
+        result += " ";
+        break;
+      case 9:
+        result += " ";
+        break;
+      default:
+        break;
     }
-    //
-    phoneInput.value = result;
-
+    result += value[i];
+  }
+  //
+  phoneInput.value = result;
 });
- const invalidError = document.querySelector(".error-invalid")
-
+const invalidError = document.querySelector(".error-invalid");
 
 phoneInput.addEventListener("focusout", function () {
-
-    console.log(phoneInput.value.length)
-    if ( phoneInput.value.length <16) {
-        invalidError.classList.add("open")
-    }
-    else {
-        invalidError.classList.remove("open")
-    }
-})
-
-
+  console.log(phoneInput.value.length);
+  if (phoneInput.value.length < 16) {
+    invalidError.classList.add("open");
+  } else {
+    invalidError.classList.remove("open");
+  }
+});
